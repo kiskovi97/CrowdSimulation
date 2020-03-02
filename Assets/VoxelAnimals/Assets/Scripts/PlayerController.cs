fee54b50044af861ae98b0fed11e202613f220ae
kiskovi97 <kiskovi97@gmail.com>
2020-03-02 21:29:11 +0100

    Animals

---
+++ Assets/VoxelAnimals/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed = 3;
+    public float jumpForce = 300;
+    public float timeBeforeNextJump = 1.2f;
+    private float canJump = 0f;
+    Animator anim;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ControllPlayer();
+    }
+
+    void ControllPlayer()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            anim.SetInteger("Walk", 1);
+        }
+        else {
+            anim.SetInteger("Walk", 0);
+        }
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+
+        if (Input.GetButtonDown("Jump") && Time.time > canJump)
+        {
+                rb.AddForce(0, jumpForce, 0);
+                canJump = Time.time + timeBeforeNextJump;
+                anim.SetTrigger("jump");
+        }
+    }
+}
