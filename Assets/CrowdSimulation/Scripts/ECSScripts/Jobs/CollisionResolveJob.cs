8aa1a2680f4c435e1837933580e5143ab479e351
kiskovi97 <kiskovi97@gmail.com>
2020-03-25 22:02:38 +0100

    CollisionResolve

--- Assets/CrowdSimulation/Scripts/ECSScripts/Jobs/CollisionResolveJob.cs
+++ Assets/CrowdSimulation/Scripts/ECSScripts/Jobs/CollisionResolveJob.cs
@@ -11,11 +11,11 @@ public struct CollisionResolveJob : IJobForEach<Translation, Walker, CollisionPa
 {
     [NativeDisableParallelForRestriction]
     [ReadOnly]
-    public NativeMultiHashMap<int, QuadrantData> targetMap;
+    public NativeMultiHashMap<int, EntitiesHashMap.MyData> targetMap;
 
     [NativeDisableParallelForRestriction]
     [ReadOnly]
-    public NativeArray<MyCollider> colliders;
+    public NativeMultiHashMap<int, CollidersHashMap.MyData> colliders;
 
     public void Execute(ref Translation translation, ref Walker walker, [ReadOnly] ref CollisionParameters collision)
     {
@@ -23,41 +23,52 @@ public struct CollisionResolveJob : IJobForEach<Translation, Walker, CollisionPa
         ForeachAround(new QuadrantData() { direction = walker.direction, position = translation.Value , radius = collision.innerRadius},
             ref correction);
 
-        for (int i=0; i<colliders.Length; i++)
-        {
-            var collider = colliders[i];
-            var localPos = translation.Value - collider.localToWorld.Position;
-            localPos = math.mul(math.inverse(collider.localToWorld.Rotation), localPos);
+        ForeachAround2(collision, translation, ref correction);
 
-            var aab = collider.collider.Value.Value.CalculateAabb();
-            var distance = math.length(aab.Max - aab.Min);
 
-            if (math.length(localPos) < distance)
-            {
-                if (collider.collider.Value.Value.CalculateDistance(new PointDistanceInput()
-                {
-                    Position = localPos,
-                    MaxDistance = float.MaxValue,
-                    Filter = CollisionFilter.Default
-                }, out DistanceHit hit))
-                {
-                    if (hit.Distance < collision.innerRadius * 2)
-                    {
-                        var normal = math.mul(collider.localToWorld.Rotation, hit.SurfaceNormal);
-                        normal.y = 0;
-                        correction += math.normalize(normal) * (collision.innerRadius * 2 - hit.Distance + 0.1f);
-                    }
-                }
-            }
-
-           
-
-        }
         translation.Value += correction * 0.1f;
 
         if (math.length(correction) > 0.1f)
         {
             walker.direction += correction;
+        }
+    }
+
+    private void ColliderResolve(CollidersHashMap.MyData collider, CollisionParameters collision, Translation translation, ref float3 correction)
+    {
+        var localPos = translation.Value - collider.data2.Position;
+        localPos = math.mul(math.inverse(collider.data2.Rotation), localPos);
+
+        var aab = collider.data.Value.Value.CalculateAabb();
+        var distance = math.length(aab.Max - aab.Min);
+
+        if (math.length(localPos) < distance)
+        {
+            if (collider.data.Value.Value.CalculateDistance(new PointDistanceInput()
+            {
+                Position = localPos,
+                MaxDistance = float.MaxValue,
+                Filter = CollisionFilter.Default
+            }, out DistanceHit hit))
+            {
+                if (hit.Distance < collision.innerRadius * 2)
+                {
+                    var normal = math.mul(collider.data2.Rotation, hit.SurfaceNormal);
+                    normal.y = 0;
+                    correction += math.normalize(normal) * (collision.innerRadius * 2 - hit.Distance + 0.1f);
+                }
+            }
+        }
+    }
+
+    private void InForeach(EntitiesHashMap.MyData other, QuadrantData me, ref float3 avoidanceForce)
+    {
+        var direction = me.position - other.position;
+        var length = math.length(direction);
+        if (length > 0.1f)
+        {
+            var distance = math.max(0f, me.radius + other.data3.innerRadius - length);
+            avoidanceForce += distance * math.normalize(direction);
         }
     }
 
@@ -86,23 +97,53 @@ public struct CollisionResolveJob : IJobForEach<Translation, Walker, CollisionPa
         Foreach(key, me, ref correction);
     }
 
+    private void ForeachAround2(CollisionParameters collision, Translation translation, ref float3 correction)
+    {
+        var position = translation.Value;
+        var key = QuadrantVariables.GetPositionHashMapKey(position);
+        Foreach(key, collision, translation, ref correction);
+        key = QuadrantVariables.GetPositionHashMapKey(position, new float3(1, 0, 0));
+        Foreach(key, collision, translation, ref correction);
+        key = QuadrantVariables.GetPositionHashMapKey(position, new float3(-1, 0, 0));
+        Foreach(key, collision, translation, ref correction);
+        key = QuadrantVariables.GetPositionHashMapKey(position, new float3(0, 0, 1));
+        Foreach(key, collision, translation, ref correction);
+        key = QuadrantVariables.GetPositionHashMapKey(position, new float3(0, 0, -1));
+        Foreach(key, collision, translation, ref correction);
+
+
+        key = QuadrantVariables.GetPositionHashMapKey(position, new float3(1, 0, 1));
+        Foreach(key, collision, translation, ref correction);
+        key = QuadrantVariables.GetPositionHashMapKey(position, new float3(-1, 0, 1));
+        Foreach(key, collision, translation, ref correction);
+        key = QuadrantVariables.GetPositionHashMapKey(position, new float3(-1, 0, -1));
+        Foreach(key, collision, translation, ref correction);
+        key = QuadrantVariables.GetPositionHashMapKey(position, new float3(1, 0, -1));
+        Foreach(key, collision, translation, ref correction);
+    }
+
 
     private void Foreach(int key, QuadrantData me, ref float3 avoidanceForce)
     {
-        if (targetMap.TryGetFirstValue(key, out QuadrantData other, out NativeMultiHashMapIterator<int> iterator))
+        if (targetMap.TryGetFirstValue(key, out EntitiesHashMap.MyData other, out NativeMultiHashMapIterator<int> iterator))
         {
             do
             {
-                var direction = me.position - other.position;
-                var length = math.length(direction);
-                if (length > 0.1f)
-                {
-                    var distance = math.max(0f, me.radius + other.radius - length);
-                    avoidanceForce += distance * math.normalize(direction);
-                }
-
-
+                InForeach(other, me, ref avoidanceForce);
             } while (targetMap.TryGetNextValue(out other, ref iterator));
         }
     }
+
+    private void Foreach(int key, CollisionParameters collision, Translation translation, ref float3 correction)
+    {
+        if (colliders.TryGetFirstValue(key, out CollidersHashMap.MyData other, out NativeMultiHashMapIterator<int> iterator))
+        {
+            do
+            {
+                ColliderResolve(other, collision, translation, ref correction);
+            } while (colliders.TryGetNextValue(out other, ref iterator));
+        }
+    }
+
+
 }
--- Assets/CrowdSimulation/Scripts/ECSScripts/Systems/CollisionSystem.cs
+++ Assets/CrowdSimulation/Scripts/ECSScripts/Systems/CollisionSystem.cs
@@ -6,63 +6,18 @@ using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
 
-public struct MyCollider
-{
-    [ReadOnly]
-    public PhysicsCollider collider;
-    [ReadOnly]
-    public LocalToWorld localToWorld;
-}
-
 [AlwaysSynchronizeSystem]
 [UpdateAfter(typeof(QuadrantSystem))]
 public class CollisionSystem : JobComponentSystem
 {
-    [NativeDisableParallelForRestriction]
-    public static NativeArray<MyCollider> colliders;
-
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        EntityQuery entityQuery = GetEntityQuery(typeof(LocalToWorld), typeof(PhysicsCollider));
-        var count = entityQuery.CalculateEntityCount();
-        if (count != CollisionSystem.colliders.Length)
-        {
-            CollisionSystem.colliders.Dispose();
-            CollisionSystem.colliders = new NativeArray<MyCollider>(count, Allocator.Persistent);
-        }
-
-        var locals = entityQuery.ToComponentDataArray<LocalToWorld>(Allocator.TempJob);
-        var colliders = entityQuery.ToComponentDataArray<PhysicsCollider>(Allocator.TempJob);
-        for (int i=0; i< count; i++)
-        {
-            CollisionSystem.colliders[i] = new MyCollider()
-            {
-                localToWorld = locals[i],
-                collider = colliders[i]
-            };
-        }
-        locals.Dispose();
-        colliders.Dispose();
-
         var collisionForce = new CollisionResolveJob() {
-            targetMap = QuadrantSystem.quadrantHashMap,
-            colliders = CollisionSystem.colliders
+            targetMap = EntitiesHashMap.quadrantHashMap,
+            colliders = CollidersHashMap.quadrantHashMap,
         };
         var collisionHandle = collisionForce.Schedule(this, inputDeps);
 
         return collisionHandle;
-    }
-
-    protected override void OnCreate()
-    {
-        colliders = new NativeArray<MyCollider>(0, Allocator.Persistent);
-        base.OnCreate();
-    }
-
-
-    protected override void OnDestroy()
-    {
-        colliders.Dispose();
-        base.OnDestroy();
     }
 }
