d76ffca95afe062730e50c058b27b56fbee95e03
kiskovi97 <kiskovi97@gmail.com>
2020-03-28 16:16:59 +0100

    Destroy THe Mesh too

---
+++ Assets/CrowdSimulation/Scripts/ECSScripts/Systems/NoParentDestroySystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class NoParentDestroySystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        var eqd = new EntityQueryDesc
+        {
+            None = new ComponentType[] { typeof(Parent) },
+            All = new ComponentType[] { typeof(NoParentDestroy) }
+        };
+        var query = GetEntityQuery(eqd);
+        var entities = query.ToEntityArray(Allocator.TempJob);
+        for (int i = 0; i < entities.Length; i++)
+        {
+            var entity = entities[i];
+            DestroyChild(entity);
+        }
+        entities.Dispose();
+        EntityManager.DestroyEntity(query);
+    }
+
+    private void DestroyChild(Entity entity)
+    {
+        if (!EntityManager.HasComponent<Child>(entity)) return;
+        var children = EntityManager.GetBuffer<Child>(entity);
+
+        NativeArray<Entity> entities = new NativeArray<Entity>(children.Length, Allocator.TempJob);
+        for (int i = 0; i < children.Length; i++)
+        {
+            var child = children[i];
+            DestroyChild(child.Value);
+            entities[i] = child.Value;
+        }
+        EntityManager.DestroyEntity(entities);
+        entities.Dispose();
+    }
+}
+
