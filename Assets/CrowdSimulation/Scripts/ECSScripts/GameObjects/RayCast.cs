956280071619289f4388899767630813865710a7
kiskovi97 <kiskovi97@gmail.com>
2020-04-11 07:58:08 +0200

    Rest Position With RayCast

--- Assets/CrowdSimulation/Scripts/ECSScripts/GameObjects/RayCast.cs
+++ Assets/CrowdSimulation/Scripts/ECSScripts/GameObjects/RayCast.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class RayCast : MonoBehaviour
 {
@@ -31,7 +33,8 @@ public class RayCast : MonoBehaviour
         {
             Entity hitEntity = buildPhysicsWorld.PhysicsWorld.Bodies[hit.RigidBodyIndex].Entity;
             return hitEntity;
-        } else
+        }
+        else
         {
             return Entity.Null;
         }
@@ -45,18 +48,59 @@ public class RayCast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (IsPointerOverUIObject()) return;
         if (Input.GetMouseButtonDown(0))
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             float distance = 50f;
             var entity = Raycast(ray.origin, ray.origin + ray.direction * distance);
-
+            if (entity == Entity.Null)
+            {
+                MonoBehaviourRayCast(ray);
+                return;
+            }
             var em = World.DefaultGameObjectInjectionWorld.EntityManager;
             if (em.HasComponent<Selection>(entity))
             {
                 var selection = em.GetComponentData<Selection>(entity);
                 selection.Selected = !selection.Selected;
                 em.SetComponentData(entity, selection);
+            }
+        }
+    }
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+
+    private void MonoBehaviourRayCast(UnityEngine.Ray ray)
+    {
+        if (GUIUtility.hotControl != 0) return;
+        if (Physics.Raycast(ray, out UnityEngine.RaycastHit hit, 100f))
+        {
+            var goalPoint = hit.point;
+
+            var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+            var all = em.GetAllEntities(Unity.Collections.Allocator.Temp);
+            foreach (var entity in all)
+            {
+                if (em.HasComponent<Selection>(entity))
+                {
+                    var selection = em.GetComponentData<Selection>(entity);
+                    if (selection.Selected)
+                    {
+                        if (em.HasComponent<Fighter>(entity))
+                        {
+                            var fighter = em.GetComponentData<Fighter>(entity);
+                            fighter.restPos = goalPoint;
+                            em.SetComponentData(entity, fighter);
+                        }
+                    }
+                }
             }
         }
     }
