0964a632e4b64ef7581214eaa47f844fa306bc82
kiskovi97 <kiskovi97@gmail.com>
2020-04-17 21:25:56 +0200

    UI First

---
+++ Assets/CrowdSimulation/Scripts/RotatingObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * speed));
+    }
+}
