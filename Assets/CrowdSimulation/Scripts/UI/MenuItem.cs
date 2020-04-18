40ccd691024c7890635b7085a7caa7d78d48033f
kiskovi97 <kiskovi97@gmail.com>
2020-04-18 19:46:30 +0200

    UI Update

---
+++ Assets/CrowdSimulation/Scripts/UI/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItem : MonoBehaviour
+{
+    public GameObject menuWindow;
+    public GameObject backgroundBlur;
+
+    public bool open = false;
+
+    public void OnClick()
+    {
+        open = !open;
+        menuWindow.SetActive(open);
+        backgroundBlur.SetActive(open);
+    }
+
+}
