2fa31ec8614bd2483acfb3beea2b1c5b6afe35e6
kiskovi97 <kiskovi97@gmail.com>
2020-04-19 15:53:47 +0200

    UI Update

--- Assets/CrowdSimulation/Scripts/UI/LoadingUI.cs
+++ Assets/CrowdSimulation/Scripts/UI/LoadingUI.cs
@@ -2,24 +2,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 namespace Assets.CrowdSimulation.Scripts.UI
 {
     public class LoadingUI : MonoBehaviour
     {
         public Transform loadingCircle;
+        public Image loadingCircleImg;
+        public Image[] colorables;
+        public TextMeshProUGUI text;
         public float loading = 1f;
+
+        public Color[] colors;
 
         private Entity entity;
 
         void Update()
         {
-            loadingCircle.localScale = new Vector3(loading, loading, loading);
+            loadingCircleImg.fillAmount = loading;
             var em = World.DefaultGameObjectInjectionWorld.EntityManager;
             if (em.HasComponent<Translation>(entity))
             {
@@ -34,6 +41,8 @@ namespace Assets.CrowdSimulation.Scripts.UI
             {
                 var spawn = em.GetComponentData<SpawnerParameters>(entity);
                 loading = 1f - spawn.spawnTimer / spawn.spawnTime;
+
+                text.text = $"{spawn.currentEntity} / {spawn.maxEntity}";
             }
         }
 
@@ -51,6 +60,15 @@ namespace Assets.CrowdSimulation.Scripts.UI
         internal void SetEntity(Entity entity)
         {
             this.entity = entity;
+            var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+            if (em.HasComponent<SpawnerParameters>(entity))
+            {
+                var spawn = em.GetComponentData<SpawnerParameters>(entity);
+                foreach (var image in colorables)
+                {
+                    image.color = colors[spawn.groupId];
+                }
+            }
         }
     }
 }
