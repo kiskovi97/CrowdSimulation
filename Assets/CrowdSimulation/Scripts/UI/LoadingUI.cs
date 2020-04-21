a7d599707ed5e0ce8f204dafb441617fa764b889
kiskovi97 <kiskovi97@gmail.com>
2020-04-21 09:07:11 +0200

    Multiple UI

--- Assets/CrowdSimulation/Scripts/UI/LoadingUI.cs
+++ Assets/CrowdSimulation/Scripts/UI/LoadingUI.cs
@@ -14,11 +14,10 @@ namespace Assets.CrowdSimulation.Scripts.UI
 {
     public class LoadingUI : MonoBehaviour
     {
-        public Transform loadingCircle;
-        public Image loadingCircleImg;
+        public Image[] loadingCircleImg;
         public Image[] colorables;
-        public TextMeshProUGUI text;
-        public float loading = 1f;
+        public TextMeshProUGUI[] text;
+        public float[] loading;
 
         public Color[] colors;
 
@@ -26,27 +25,32 @@ namespace Assets.CrowdSimulation.Scripts.UI
 
         void Update()
         {
-            loadingCircleImg.fillAmount = loading;
             var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+
             if (em.HasComponent<Translation>(entity))
             {
                 var translate = em.GetComponentData<Translation>(entity);
                 var pos = Camera.main.WorldToScreenPoint(translate.Value + new float3(0, 2, 0));
                 transform.position = pos;
-            } else
+            }
+            else
             {
                 transform.position = new Vector2(0, Camera.main.pixelHeight);
             }
-            if (em.HasComponent<SpawnerParameters>(entity))
-            {
-                var spawn = em.GetComponentData<SpawnerParameters>(entity);
-                loading = 1f - spawn.spawnTimer / spawn.spawnTime;
 
-                text.text = $"{spawn.currentEntity} / {spawn.maxEntity}";
+            for ( int id = 0; id < loading.Length; id++)
+            {
+                loadingCircleImg[id].fillAmount = loading[id];
+                if (em.HasComponent<SpawnerParameters>(entity))
+                {
+                    var spawn = em.GetComponentData<SpawnerParameters>(entity);
+                    loading[id] = 1f - spawn.spawnTimer / spawn.spawnTime;
+                    text[id].text = $"{spawn.currentEntity} / {spawn.maxEntity}";
+                }
             }
         }
 
-        public void NewPeople()
+        public void NewPeople(int id)
         {
             var em = World.DefaultGameObjectInjectionWorld.EntityManager;
             if (em.HasComponent<SpawnerParameters>(entity))
