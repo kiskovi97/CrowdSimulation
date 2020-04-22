4d4daed621174f34c429186c689510136cd7447b
kiskovi97 <kiskovi97@gmail.com>
2020-04-22 11:08:46 +0200

    Clear Prefabs

--- Assets/CrowdSimulation/Scripts/ECSScripts/ComponentDatas/Selection.cs
+++ Assets/CrowdSimulation/Scripts/ECSScripts/ComponentDatas/Selection.cs
@@ -8,7 +8,9 @@ namespace Assets.CrowdSimulation.Scripts.ECSScripts.ComponentDatas
     [GenerateAuthoringComponent]
     public struct Selection : IComponentData
     {
+        [System.NonSerialized]
         public bool Selected;
+        [System.NonSerialized]
         public bool changed;
     }
 }
