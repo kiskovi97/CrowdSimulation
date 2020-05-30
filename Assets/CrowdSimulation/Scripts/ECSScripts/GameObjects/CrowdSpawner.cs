9826de38577e18096e3b27812d37e37458e90e83
kiskovi97 <kiskovi97@gmail.com>
2020-05-30 10:09:52 +0200

    Refactor Crowd Spawner and Move Scenes

--- Assets/CrowdSimulation/Scripts/ECSScripts/GameObjects/CrowdSpawner.cs
+++ Assets/CrowdSimulation/Scripts/ECSScripts/GameObjects/CrowdSpawner.cs
@@ -15,7 +15,9 @@ namespace Assets.CrowdSimulation.Scripts.ECSScripts.GameObjects
         public GameObject entityObject;
         public Transform goalPoint;
         [SerializeField]
-        public PathFindingData data;
+        public PathFindingMethod pathFindingMethod;
+        [SerializeField]
+        public DecisionMethod decisionMethod;
         public TextMeshProUGUI info;
         public Material color;
 
@@ -100,7 +102,7 @@ namespace Assets.CrowdSimulation.Scripts.ECSScripts.GameObjects
                     var person = obj.GetComponent<PersonObject>();
                     if (person != null)
                     {
-                        person.ChangeGroup(cond, Id, data);
+                        person.ChangeGroup(cond, Id, new PathFindingData() { pathFindingMethod = pathFindingMethod, decisionMethod = decisionMethod });
                         person.ConnectParent(this);
                     }
                 }
@@ -111,7 +113,7 @@ namespace Assets.CrowdSimulation.Scripts.ECSScripts.GameObjects
         {
             if (info != null)
             {
-                info.text = "PathFinding: " + data.pathFindingMethod.ToString() + ", Decision: " + data.decisionMethod.ToString();
+                info.text = "PathFinding: " + pathFindingMethod.ToString() + ", Decision: " + decisionMethod.ToString();
                 info.color = color.color;
             }
 
