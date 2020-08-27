6ade1d2d12ffd1717558e7c9878c03ed93e76b48
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2020-08-27 10:46:06 +0200

    RandomCat JobChunk

---
+++ Assets/CrowdSimulation/Scripts/ECSScripts/JobChunks/RandomCatJob.cs
@@ -0,0 +1,94 @@
+﻿using Unity.Entities;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Assets.CrowdSimulation.Scripts.ECSScripts.ComponentDatas;
+using Unity.Burst;
+
+namespace Assets.CrowdSimulation.Scripts.ECSScripts.JobChunks
+{
+    [BurstCompile]
+    public struct RandomCatJob : IJobChunk
+    {
+        public Random random;
+        public float deltaTime;
+
+        public ComponentTypeHandle<Walker> WalkerHandle;
+        [ReadOnly] public ComponentTypeHandle<RandomCat> RandomCatHandle;
+        public ComponentTypeHandle<Translation> TranslationHandle;
+
+        public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+        {
+            var walkers = chunk.GetNativeArray(WalkerHandle);
+            var randomCats = chunk.GetNativeArray(RandomCatHandle);
+            var translations = chunk.GetNativeArray(TranslationHandle);
+
+            for (var i = 0; i < chunk.Count; i++)
+            {
+                var walker = walkers[i];
+                var randomCat = randomCats[i];
+                var translation = translations[i];
+
+                Execute(ref randomCat, ref walker, ref translation);
+
+                walkers[i] = walker;
+                translations[i] = translation;
+            }
+        }
+
+        public void Execute([ReadOnly] ref RandomCat randomCat, ref Walker walker, ref Translation translation)
+        {
+            if (random.NextFloat() < deltaTime * randomCat.random)
+            {
+                var dir = random.NextFloat2Direction();
+                walker.force = new float3(dir.x, 0, dir.y);
+            }
+        }
+    }
+
+    [BurstCompile]
+    public struct RandomCatGroupJob : IJobChunk
+    {
+        public Random random;
+        public float deltaTime;
+
+        public ComponentTypeHandle<Walker> WalkerHandle;
+        [ReadOnly] public ComponentTypeHandle<RandomCat> RandomCatHandle;
+        public ComponentTypeHandle<Translation> TranslationHandle;
+        public ComponentTypeHandle<GroupCondition> GroupConditionHandle;
+
+        public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+        {
+            var walkers = chunk.GetNativeArray(WalkerHandle);
+            var randomCats = chunk.GetNativeArray(RandomCatHandle);
+            var translations = chunk.GetNativeArray(TranslationHandle);
+            var groupConditions = chunk.GetNativeArray(GroupConditionHandle);
+
+            for (var i = 0; i < chunk.Count; i++)
+            {
+                var walker = walkers[i];
+                var randomCat = randomCats[i];
+                var translation = translations[i];
+                var groupCondition = groupConditions[i];
+
+                Execute(ref randomCat, ref walker, ref translation, ref groupCondition);
+
+                walkers[i] = walker;
+                translations[i] = translation;
+                groupConditions[i] = groupCondition;
+            }
+        }
+
+        public void Execute([ReadOnly] ref RandomCat randomCat, ref Walker walker, ref Translation translation, ref GroupCondition groupCondition)
+        {
+            if (random.NextFloat() < deltaTime * randomCat.random)
+            {
+
+                var pos = groupCondition.goalPoint - translation.Value;
+
+                var distance = math.length(pos);
+                walker.force += deltaTime * distance * pos * 0.03f;
+            }
+        }
+    }
+}
--- Assets/CrowdSimulation/Scripts/ECSScripts/Systems/RandomCatSystem.cs
+++ Assets/CrowdSimulation/Scripts/ECSScripts/Systems/RandomCatSystem.cs
@@ -1,12 +1,35 @@
-﻿using Assets.CrowdSimulation.Scripts.ECSScripts.Jobs;
+﻿using Assets.CrowdSimulation.Scripts.ECSScripts.ComponentDatas;
+using Assets.CrowdSimulation.Scripts.ECSScripts.JobChunks;
+using Assets.CrowdSimulation.Scripts.ECSScripts.Jobs;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
+using Unity.Transforms;
 
 namespace Assets.CrowdSimulation.Scripts.ECSScripts.Systems
 {
     public class RandomCatSystem : JobComponentSystem
     {
+        private EntityQuery randomCatEntities;
+        private EntityQuery randomCatGroupEntities;
+        protected override void OnCreate()
+        {
+            var walkingQuery = new EntityQueryDesc
+            {
+                All = new ComponentType[] { typeof(Walker), typeof(RandomCat), typeof(Translation) },
+            };
+            randomCatEntities = GetEntityQuery(walkingQuery);
+
+
+            var groupQuery = new EntityQueryDesc
+            {
+                All = new ComponentType[] { typeof(Walker), typeof(RandomCat), typeof(Translation), typeof(GroupCondition) },
+            };
+            randomCatGroupEntities = GetEntityQuery(groupQuery);
+
+            base.OnCreate();
+        }
+
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
             var deltaTime = Time.DeltaTime;
@@ -16,18 +39,25 @@ namespace Assets.CrowdSimulation.Scripts.ECSScripts.Systems
             var forceJob = new RandomCatJob()
             {
                 deltaTime = deltaTime,
-                random = random
+                random = random,
+                RandomCatHandle = GetComponentTypeHandle<RandomCat>(true),
+                TranslationHandle = GetComponentTypeHandle<Translation>(),
+                WalkerHandle = GetComponentTypeHandle<Walker>(),
             };
 
-            var forceHandle = forceJob.Schedule(this, inputDeps);
+            var forceHandle = forceJob.Schedule(randomCatEntities, inputDeps);
 
             var forceJob2 = new RandomCatGroupJob()
             {
                 deltaTime = deltaTime,
-                random = random
+                random = random,
+                RandomCatHandle = GetComponentTypeHandle<RandomCat>(true),
+                TranslationHandle = GetComponentTypeHandle<Translation>(),
+                WalkerHandle = GetComponentTypeHandle<Walker>(),
+                GroupConditionHandle = GetComponentTypeHandle<GroupCondition>(),
             };
 
-            var forceHandle2 = forceJob2.Schedule(this, forceHandle);
+            var forceHandle2 = forceJob2.Schedule(randomCatGroupEntities, forceHandle);
 
             return forceHandle2;
         }
