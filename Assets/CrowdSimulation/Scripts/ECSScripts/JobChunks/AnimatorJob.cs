0da244dc745d76af2fcfed134fa8e02a4d318d4b
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2020-11-14 15:46:48 +0100

    Fix

--- Assets/CrowdSimulation/Scripts/ECSScripts/JobChunks/AnimatorJob.cs
+++ Assets/CrowdSimulation/Scripts/ECSScripts/JobChunks/AnimatorJob.cs
@@ -4,6 +4,7 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using static Assets.CrowdSimulation.Scripts.ECSScripts.Systems.AnimatorSystem;
 
 namespace Assets.CrowdSimulation.Scripts.ECSScripts.JobChunks
 {
